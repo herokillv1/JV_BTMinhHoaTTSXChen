114af7ba18288172172b2db5a8846ae66f369093
herokillv1 <nguyentrunghieu.hha101196@gmail.com>
2020-07-23 11:30:23 +0700

    [Bài tập] Minh hoạ thuật toán sắp xếp chèn

---
+++ src/InsertionSort.java
@@ -0,0 +1,29 @@
+public class InsertionSort {
+    public static void main(String[] args) {
+         int[] arr = {12,1,9,4,6,5,10,-4};
+
+        for (int j =0 ; j< arr.length;j++){
+            System.out.print(arr[j]+"   ");
+        }
+        System.out.println();
+
+         for (int i=0;i<arr.length;i++){
+             int X = arr[i];
+             int k ;
+             for (k=i-1;k>=0 && arr[k] > X;k--){
+                 arr[k+1] = arr[k];
+             }
+
+             arr[k+1] = X;
+             System.out.println();
+
+             for (int j =0 ; j< arr.length;j++){
+                 System.out.print(arr[j]+"   ");
+             }
+             System.out.println();
+         }
+
+
+    }
+
+}
